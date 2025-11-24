7bc06bc2c1139194d9d864175b4901b505ec1e23
MichaelRooney99 <msroone@gmail.com>
2025-11-23 23:23:49 -0600

    ads

--- main.jsx
+++ main.jsx
@@ -1,5 +1,5 @@
 import { createRoot } from "react-dom/client";
-import "./src/task-manager.css";
-import TaskManager from "./src/task-manager.jsx";
+import "./task-manager.css";
+import TaskManager from "./task-manager.jsx";
 
 createRoot(document.getElementById("root")).render(<TaskManager />);
