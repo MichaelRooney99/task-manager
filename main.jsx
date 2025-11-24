ae1800baa5445951d348349be73aecc85b9a48b3
MichaelRooney99 <msroone@gmail.com>
2025-11-23 22:59:14 -0600

    fas

--- main.jsx
+++ main.jsx
@@ -1,7 +1,7 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import "./task-manager.css";
-import TaskManager from "./task-manager.jsx";
+import "./src/task-manager.css";
+import TaskManager from "./src/task-manager.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
