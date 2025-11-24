bf5187be2d7cd36d4673432e921ae783eebdd7b3
MichaelRooney99 <msroone@gmail.com>
2025-11-23 23:18:16 -0600

    tr

--- main.jsx
+++ main.jsx
@@ -1,10 +1,5 @@
-import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./src/task-manager.css";
 import TaskManager from "./src/task-manager.jsx";
 
-createRoot(document.getElementById("root")).render(
-  <StrictMode>
-    <TaskManager />
-  </StrictMode>
-);
+createRoot(document.getElementById("root")).render(<TaskManager />);
