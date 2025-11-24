7cc991110d5f5b1a81496b652b3fcfcb1dc96d7b
MichaelRooney99 <msroone@gmail.com>
2025-11-23 22:45:09 -0600

    fsd

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "https://michaelrooney99.github.io/task-manager/"
+  base: "/task-manager/"
 });
